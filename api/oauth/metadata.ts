d2cba54d05398f1d80df69628c0ce884c7e9b773
Victor Ronin <victor.ronin@gmail.com>
2026-03-13 15:27:06 -0400

    removing extras

--- api/oauth/metadata.ts
+++ api/oauth/metadata.ts
@@ -11,10 +11,10 @@ app.use((req: Request, res: Response) => {
     token_endpoint: `${issuer}/oauth/token`,
     registration_endpoint: `${issuer}/oauth/register`,
     response_types_supported: ["code"],
-    grant_types_supported: ["authorization_code", "client_credentials", "refresh_token"],
-    code_challenge_methods_supported: ["S256", "plain"],
-    token_endpoint_auth_methods_supported: ["none", "client_secret_basic", "client_secret_post"],
-    scopes_supported: ["offline_access"],
+    grant_types_supported: ["authorization_code", "refresh_token"],
+    code_challenge_methods_supported: ["S256"],
+    token_endpoint_auth_methods_supported: ["none", "client_secret_post"],
+    scopes_supported: ["read", "write"],
   });
 });
 
