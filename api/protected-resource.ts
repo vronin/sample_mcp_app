605448babd4deae0c92a191407496a944dfabed7
Victor Ronin <victor.ronin@gmail.com>
2026-03-13 14:00:58 -0400

    log

--- api/protected-resource.ts
+++ api/protected-resource.ts
@@ -6,6 +6,7 @@ const app = express();
 
 
 app.use((_req: Request, res: Response) => {
+  console.info("Request received for protected resource. Returning scopes: openid, profile, email");
   res.json({
     resource: "https://sample-mcp-pttgpnn83-aembit.vercel.app",
     authorization_servers: ["https://22a7a6.mcp.qa.aembit-eng.com"],
