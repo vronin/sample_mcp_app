dc43139955a4489b9806c7f8032bff011c049d19
Victor Ronin <victor.ronin@gmail.com>
2026-03-13 15:12:14 -0400

    Back to Auth server on board

--- api/protected-resource.ts
+++ api/protected-resource.ts
@@ -9,8 +9,8 @@ app.use((_req: Request, res: Response) => {
   console.info("Request received for protected resource. Returning scopes: openid, profile, email");
   res.json({
     resource: "https://sample-mcp-app.vercel.app/mcp",
-    //authorization_servers: ["https://sample-mcp-app.vercel.app"],
-    authorization_servers: ["https://22a7a6.mcp.qa.aembit-eng.com"],
+    authorization_servers: ["https://sample-mcp-app.vercel.app"],
+    //authorization_servers: ["https://22a7a6.mcp.qa.aembit-eng.com"],
     scopes_supported: ["openid", "profile", "email"],
     bearer_methods_supported: ["header"],
   });
